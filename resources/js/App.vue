06303adf2cfc1d910a4d1e026cf489e246970ae9
Max Eakkabin Jaikeawma <eakkabin.max@outlook.com>
2024-03-28 15:26:35 +0700

    update

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -8,20 +8,8 @@
 
   export default {
     name: 'App',
-    // props: {},
-    // components: { },
-    // data() {return {}},
-    // mounted: async function() {},
-    // methods: {}
   }
 
 </script>
 
-<style>
-/*
-@media (min-width: 768px) {}
-@media (min-width: 992px) {}
-@media (min-width: 1200px) {}
-@media (min-width: 1440px) {}
-*/
-</style>
+<style></style>
